72fcc3438aff542e0db77ff64ca9635f93db2896
Binjobo <101441601+Binjobo@users.noreply.github.com>
2023-11-29 14:30:27 +0800

    2nd commit on weds

--- flappy-bird/main.js
+++ flappy-bird/main.js
@@ -5,7 +5,6 @@
 let xAxis = 0;
 let yAxis = 0;
 let started = false;
-// let gameOver = false;
 
 /*----- cached elements  -----*/
 let gameScreen = document.querySelector("body");
@@ -17,15 +16,15 @@ const clawPair1 = document.querySelector("#pair1");
 
 const clawDownLong = document.querySelector("#clawDownLong");
 const clawUpShort = document.querySelector("#clawUpShort");
-
 const clawDownShort = document.querySelector("#clawDownShort");
 const clawUpLong = document.querySelector("#clawUpLong");
-
 const clawPair3 = document.querySelector("#pair3");
 const clawPair4 = document.querySelector("#pair4");
 
 const startScreen = document.querySelector("#start");
 startScreen.style.display = "block";
+
+const scores = document.querySelector("#scoreNumbers");
 
 const gameOverScreen = document.querySelector("#gameOver");
 gameOverScreen.style.display = "none";
@@ -35,19 +34,12 @@ const startOverButton = document.querySelector("#restart");
 /*----- event listeners -----*/
 gameScreen.addEventListener("click", gameStart);
 gameScreen.addEventListener("keydown", gameStart);
-
-gameScreen.addEventListener("keydown", moveClawsLeftKey);
-gameScreen.addEventListener("keydown", moveClawsRightKey);
-
 gameScreen.addEventListener("keydown", moveUpKey);
-
 startOverButton.addEventListener("click", startOver);
 
 /*----- functions -----*/
 
 //bird 
-let enableMoveUp = true;
-
 function moveUpKey(e) {
     if (parseInt(bird.style.top) <= -250) {
         return;
@@ -70,30 +62,16 @@ function gravity() {
     //referenced chatGPT examples
 }
 
-
 //claws
-function moveClawsLeftKey(e) {
-    if (e.keyCode === 37) {
-        xAxis -= 20;
-        allClaws.style.left = xAxis + "px";
-    }
-}
-
-function moveClawsRightKey(e) {
-    if (e.keyCode === 39) {
-        xAxis += 20;
-        allClaws.style.left = xAxis + "px";
-    }
-}
-
-function clawsAutoMoveLeft() {
+function clawsAutoMoveLeft(callback) {
     function moveLeft() {
         if (checkAllCollisions()) {               //YESSSSSSSSSSSSSSSSSSSSSSS!!!!!  
             clearInterval(shiftLeftSpeed);
-        }else if (xAxis <= -4000) {
+        } else if (xAxis <= -4000) {
             clearInterval(shiftLeftSpeed);
             allClaws.style.left = 0;
             xAxis = 0;
+            callback();
         } else {
             xAxis -= 5;
             allClaws.style.left = xAxis + 'px';
@@ -102,219 +80,73 @@ function clawsAutoMoveLeft() {
     const shiftLeftSpeed = setInterval(moveLeft, 15);
 }
 
-     
-// function clawsAutoMoveLeft(callback) {
-//     function moveLeft() {
-//         if (xAxis <= -4000) {
-//             clearInterval(shiftLeftSpeed);
-//             allClaws.style.left = 0;
-//             xAxis = 0;
-//             callback();
-//         } else {
-//             xAxis -= 5;
-//             allClaws.style.left = xAxis + 'px';
-//         }
-//     }
-//     const shiftLeftSpeed = setInterval(moveLeft, 15);
-// }
-
-// function repeatClawsRound(rounds) {
-//     let counter = 0;
-//     function callback() {
-//         counter++;
-//         if (counter < rounds) {
-//             clawsAutoMoveLeft(callback);
-//         }
-//     }
-//     clawsAutoMoveLeft(callback);
-//     //referenced chatGPT examples
-// }
-
+function repeatClawsRound(rounds) {
+    let counter = 0;
+    function callback() {
+        counter++;
+        if (counter < rounds) {
+            clawsAutoMoveLeft(callback);
+        }
+    }
+    clawsAutoMoveLeft(callback);
+    //referenced chatGPT examples
+}
 
 //collision
-function checkCollision1() {
-    const birdRect = bird.getBoundingClientRect();
-    const clawDownLongRect = clawDownLong.getBoundingClientRect();
+function checkCollision(bird, claws) {
+    const birdSprite = bird.getBoundingClientRect();
+    const clawsPNG = claws.getBoundingClientRect();
 
-    if (birdRect.left < clawDownLongRect.left + clawDownLongRect.width &&
-        birdRect.left + birdRect.width > clawDownLongRect.left &&
-        birdRect.top < clawDownLongRect.top + clawDownLongRect.height &&
-        birdRect.top + birdRect.height > clawDownLongRect.top
+    if (
+        birdSprite.left < clawsPNG.left + clawsPNG.width &&
+        birdSprite.left + birdSprite.width > clawsPNG.left &&
+        birdSprite.top < clawsPNG.top + clawsPNG.height &&
+        birdSprite.top + birdSprite.height > clawsPNG.top
     ) {
-        // console.log("Collision with clawDownLong!");
         return true;
-    } else {
-        // console.log("No collision with clawDownLong!");
-        return false;
-    }
-}
-
-function checkCollision2() {
-    const birdRect = bird.getBoundingClientRect();
-    const clawUpShortRect = clawUpShort.getBoundingClientRect();
-
-    if (birdRect.left < clawUpShortRect.left + clawUpShortRect.width &&
-        birdRect.left + birdRect.width > clawUpShortRect.left &&
-        birdRect.top < clawUpShortRect.top + clawUpShortRect.height &&
-        birdRect.top + birdRect.height > clawUpShortRect.top
-    ) {
-        // console.log("Collision with clawUpShort!");
-        return true;
-    } else {
-        // console.log("No collision with clawUpShort!");
-        return false;
-    }
-}
-
-function checkCollision3() {
-    const birdRect = bird.getBoundingClientRect();
-    const clawDownShortRect = clawDownShort.getBoundingClientRect();
-
-    if (birdRect.left < clawDownShortRect.left + clawDownShortRect.width &&
-        birdRect.left + birdRect.width > clawDownShortRect.left &&
-        birdRect.top < clawDownShortRect.top + clawDownShortRect.height &&
-        birdRect.top + birdRect.height > clawDownShortRect.top
-    ) {
-        // console.log("Collision with clawDownShort!");
-        return true;
-    } else {
-        // console.log("No collision with clawDownShort!");
-        return false;
-    }
-}
-
-function checkCollision4() {
-    const birdRect = bird.getBoundingClientRect();
-    const clawUpLongRect = clawUpLong.getBoundingClientRect();
-
-    if (birdRect.left < clawUpLongRect.left + clawUpLongRect.width &&
-        birdRect.left + birdRect.width > clawUpLongRect.left &&
-        birdRect.top < clawUpLongRect.top + clawUpLongRect.height &&
-        birdRect.top + birdRect.height > clawUpLongRect.top
-    ) {
-        // console.log("Collision with clawUpLong!");
-        return true;
-    } else {
-        // console.log("No collision with clawUpLong!");
-        return false;
-    }
-}
-
-function checkCollision5() {
-    const birdRect = bird.getBoundingClientRect();
-    const clawPair3Rect = clawPair3.getBoundingClientRect();
-
-    if (birdRect.left < clawPair3Rect.left + clawPair3Rect.width &&
-        birdRect.left + birdRect.width > clawPair3Rect.left &&
-        birdRect.top < clawPair3Rect.top + clawPair3Rect.height &&
-        birdRect.top + birdRect.height > clawPair3Rect.top
-    ) {
-        // console.log("Collision with pair3!");
-        return true;
-    } else {
-        // console.log("No collision with pair3!");
-        return false;
-    }
-}
-
-function checkCollision6() {
-    const birdRect = bird.getBoundingClientRect();
-    const clawPair4Rect = clawPair4.getBoundingClientRect();
-
-    if (birdRect.left < clawPair4Rect.left + clawPair4Rect.width &&
-        birdRect.left + birdRect.width > clawPair4Rect.left &&
-        birdRect.top < clawPair4Rect.top + clawPair4Rect.height &&
-        birdRect.top + birdRect.height > clawPair4Rect.top
-    ) {
-        // console.log("Collision with pair4!");
-        return true;
-    } else {
-        // console.log("No collision with pair4!");
-        return false;
     }
 }
 
 function checkAllCollisions() {
     if (
         bird.offsetTop >= 640 ||    //when the bird touches the ground
-        checkCollision1() === true ||
-        checkCollision2() === true ||
-        checkCollision3() === true ||
-        checkCollision4() === true ||
-        checkCollision5() === true ||
-        checkCollision6() === true
+        checkCollision(bird, clawDownLong) ||
+        checkCollision(bird, clawUpShort) ||
+        checkCollision(bird, clawDownShort) ||
+        checkCollision(bird, clawUpLong) ||
+        checkCollision(bird, clawPair3) ||
+        checkCollision(bird, clawPair4)
     ) {
-        // console.log("Oh yeahhhh");
-        // document.querySelector("body").innerHTML = "Gameover!"
         gameOverScreen.style.display = "block";
         return true;
-        // started = false;
-        // startScreen.style.display = "none";
-
-        // startOver();
-
-
-        // function disableMoveUpKey(e) {
-        //     if (e.keyCode === 87 || e.code === "Space") {
-        //         yAxis += 30;
-        //         bird.style.top = yAxis + "px";
-        //     }
-        // }
     }
 }
 
-
-
-// function clawsAutoMoveRight() {
-//     function moveRight() {
-//         if (xAxis <= -4000) {
-//             clearInterval(shiftRightSpeed);
-//             allClaws.style.left = 0;
-//             xAxis = 0;
-//         } else {
-//             xAxis += 5;
-//             allClaws.style.left = xAxis + 'px';
-//         }
-//     }
-//     const shiftRightSpeed = setInterval(moveRight, 15);
-// }
-
-
-
-
-
-
-//function for points
-
+scores
+function scoreCount() {
+    function countingScore() {
+        if (checkAllCollisions()) {
+            clearInterval(countingScore);
+        } else {
+            scores.innerText = parseInt(scores.innerText) + 1;
+        }
+    }
+    setInterval(countingScore, 1000);
+}
 
 //start game
-
 function gameStart() {
     if(!started) {
         started = true;
         startScreen.style.display = "none";
         gravity();
-        // repeatClawsRound(3); 
-        clawsAutoMoveLeft();
+        repeatClawsRound(3); 
         setInterval(checkAllCollisions, 10);
+        scoreCount();
     }
 }
 
-// function disableMove() {
-//     gameOver = true;
-// }
-
-
-// function startOver() {
-//     if(started) {
-//         // started = false;
-//         // gameStart()
-//         console.log("Restart Game Now"); 
-//         // document.querySelector("body").innerHTML = "Gameover!"
-//         startScreen.style.display = "block";
-//     }
-// }
-
+//restart game
 function startOver() {
     if (started) {
         xAxis = 0;
@@ -322,32 +154,12 @@ function startOver() {
         started = false;
 
         bird.style.top = yAxis + "px";
-
-        // Clear the existing interval
-        clearInterval(shiftLeftSpeed);
-
         allClaws.style.left = 0;
-
         gameOverScreen.style.display = "none";
-
-        startScreen.style.display = "block";
+        // startScreen.style.display = "block";
+        scores.innerText = 0;
     }
 }
 
-// function clawsStopMoving() {
-//     function stopMove() {
-//         if (checkAllCollisions() === true) {
 
-//         }
-//         if (xAxis <= -4000) {
-//             clearInterval(shiftLeftSpeed);
-//             allClaws.style.left = 0;
-//             xAxis = 0;
-//             callback();
-//         } else {
-//             xAxis -= 5;
-//             allClaws.style.left = xAxis + 'px';
-//         }
-//     }
-//     const shiftLeftSpeed = setInterval(stopMove, 15);
-// }
+
